f26641a04cdf6673d8407acc9ce9671e5c67ff3e
Josh Crites <jc@joshcrites.com>
2023-04-26 10:24:34 -0400

    add powershell syntax

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -119,7 +119,7 @@ const config = {
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
-        additionalLanguages: ['rust'],
+        additionalLanguages: ['rust', 'powershell'],
       },
       stylesheets: [
         {
