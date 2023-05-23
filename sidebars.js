c5fefcfe83a27895a0de61d750e7e2abe62a25ea
Josh Crites <jc@joshcrites.com>
2023-05-23 11:30:50 -0400

    add black box fns page

--- sidebars.js
+++ sidebars.js
@@ -59,6 +59,7 @@ const sidebars = {
         },
         'standard_library/array_methods',
         'standard_library/field_methods',
+        'standard_library/black_box_fns',
         'standard_library/logging',
         'standard_library/merkle_trees',
         'standard_library/zeroed'
